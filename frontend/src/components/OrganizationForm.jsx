b302ae6fda2059b902476ab858a98fa6a42fd559
Axel Lundholm <axel.lundholm@me.com>
2024-08-27 22:10:38 +0200

    Created OragnizationForm component

---
+++ frontend/src/components/OrganizationForm.jsx
@@ -0,0 +1,56 @@
+import {
+  Description,
+  Field,
+  Fieldset,
+  Input,
+  Label,
+  Legend,
+  Select,
+} from "@headlessui/react";
+import { ChevronDownIcon } from "@heroicons/react/20/solid";
+import clsx from "clsx";
+
+const OrganizationForm = () => {
+  return (
+    <div className="w-full max-w-lg px-4 bg-gray-700">
+      <Fieldset className="space-y-6 rounded-xl bg-white/5 p-6 sm:p-10">
+        <Legend className="text-base/7 font-semibold text-white">
+          Organization onboarding
+        </Legend>
+        <Field>
+          <Label className="text-sm/6 font-medium text-white">Legal name</Label>
+          <Input
+            className={clsx(
+              "mt-3 block w-full rounded-lg border-none bg-white/5 py-1.5 px-3 text-sm/6 text-white",
+              "focus:outline-none data-[focus]:outline-2 data-[focus]:-outline-offset-2 data-[focus]:outline-white/25"
+            )}
+          />
+        </Field>
+        <Field>
+          <Label className="text-sm/6 font-medium text-white">Country</Label>
+          <Description className="text-sm/6 text-white/50">
+            The country where your business is registered.
+          </Description>
+          <div className="relative">
+            <Select
+              className={clsx(
+                "mt-3 block w-full appearance-none rounded-lg border-none bg-white/5 py-1.5 px-3 text-sm/6 text-white",
+                "focus:outline-none data-[focus]:outline-2 data-[focus]:-outline-offset-2 data-[focus]:outline-white/25",
+                // Make the text of each option black on Windows
+                "*:text-black"
+              )}
+            >
+              <option>Sweden</option>
+            </Select>
+            <ChevronDownIcon
+              className="group pointer-events-none absolute top-2.5 right-2.5 size-4 fill-white/60"
+              aria-hidden="true"
+            />
+          </div>
+        </Field>
+      </Fieldset>
+    </div>
+  );
+};
+
+export default OrganizationForm;
