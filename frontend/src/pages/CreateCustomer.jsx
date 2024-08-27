f54059688258ae892d788faaf3eca230bb87fae8
Axel Lundholm <axel.lundholm@me.com>
2024-08-27 22:09:38 +0200

    Changes notation

--- frontend/src/pages/CreateCustomer.jsx
+++ frontend/src/pages/CreateCustomer.jsx
@@ -39,7 +39,7 @@ const createCustomer = () => {
     <div className="p-4">
       <BackButton />
       <h1 className="text-3xl my-4">Create customer</h1>
-      {loading ? <Spinner /> : ""}
+      {loading && <Spinner />}
       <div className="flex flex-col border-2 border-sky-400 rounded-xl w-[600px] p-4 mx-auto">
         <div className="my-4">
           <label className="text-xl mr-4 text-gray-500">Type</label>
