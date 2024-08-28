44ca20a5878de5c1c554168e619d52bc2eae1d2c
Axel Lundholm <axel.lundholm@me.com>
2024-08-28 09:22:20 +0200

    Split into two organization forms

---
+++ frontend/src/components/AdvancedOrganizationForm.jsx
@@ -0,0 +1,88 @@
+import {
+  Button,
+  Description,
+  Field,
+  Fieldset,
+  Input,
+  Label,
+  Legend,
+  Select,
+} from "@headlessui/react";
+import { ArrowPathIcon, ChevronDownIcon } from "@heroicons/react/20/solid";
+import clsx from "clsx";
+
+function handleClick(e) {
+  console.log(e);
+}
+
+const AdvancedOrganizationForm = () => {
+  return (
+    <div className="w-full max-w-lg px-4">
+      <Fieldset className="space-y-6 rounded-xl p-6 sm:p-10">
+        <Legend className="text-base/7 font-semibold text-white">
+          Organization onboarding
+        </Legend>
+        <Field>
+          <Label className="text-sm/6 font-medium text-white">Legal name</Label>
+          <Description className="text-sm/6 text-white/50">
+            The registered name of your company.
+          </Description>
+          <Input
+            placeholder="AxelCorp AB"
+            className={clsx(
+              "mt-3 block w-full rounded-lg border-none bg-white/5 px-3 py-1.5 text-sm/6 text-white",
+              "focus:outline-none data-[focus]:outline-2 data-[focus]:-outline-offset-2 data-[focus]:outline-white/25",
+            )}
+          />
+        </Field>
+        <Field>
+          <Label className="text-sm/6 font-medium text-white">
+            Registration number
+          </Label>
+          <Description className="text-sm/6 text-white/50">
+            Placeholder description.
+          </Description>
+          <Input
+            placeholder="555555-5555"
+            className={clsx(
+              "mt-3 block w-full rounded-lg border-none bg-white/5 px-3 py-1.5 text-sm/6 text-white",
+              "focus:outline-none data-[focus]:outline-2 data-[focus]:-outline-offset-2 data-[focus]:outline-white/25",
+            )}
+          />
+        </Field>
+        <Field>
+          <Label className="text-sm/6 font-medium text-white">Country</Label>
+          <Description className="text-sm/6 text-white/50">
+            The country where your business is registered.
+          </Description>
+          <div className="relative">
+            <Select
+              className={clsx(
+                "mt-3 block w-full appearance-none rounded-lg border-none bg-white/5 px-3 py-1.5 text-sm/6 text-white",
+                "focus:outline-none data-[focus]:outline-2 data-[focus]:-outline-offset-2 data-[focus]:outline-white/25",
+                // Make the text of each option black on Windows
+                "*:text-black",
+              )}
+            >
+              <option value="SE">Sweden</option>
+            </Select>
+            <ChevronDownIcon
+              className="group pointer-events-none absolute right-2.5 top-2.5 size-4 fill-white/60"
+              aria-hidden="true"
+            />
+          </div>
+        </Field>
+
+        <Button
+          onClick={handleClick}
+          className="inline-flex animate-pulse items-center gap-2 rounded-md bg-gray-700 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-inner shadow-white/10 focus:outline-none data-[hover]:bg-gray-600 data-[open]:bg-gray-700 data-[focus]:outline-1 data-[focus]:outline-white"
+        >
+          <ArrowPathIcon className="h-5 w-5 animate-spin" />
+          Submit
+        </Button>
+      </Fieldset>
+    </div>
+  );
+};
+
+export default AdvancedOrganizationForm;
--- frontend/src/components/SimpleOrganizationForm.jsx
+++ frontend/src/components/SimpleOrganizationForm.jsx
@@ -1,4 +1,5 @@
 import {
+  Button,
   Description,
   Field,
   Fieldset,
@@ -7,19 +8,27 @@ import {
   Legend,
   Select,
 } from "@headlessui/react";
-import { ChevronDownIcon } from "@heroicons/react/20/solid";
+import { ArrowPathIcon, ChevronDownIcon } from "@heroicons/react/20/solid";
 import clsx from "clsx";
 
-const OrganizationForm = () => {
+function handleClick(e) {
+  console.log(e);
+}
+
+const SimpleOrganizationForm = () => {
   return (
-    <div className="w-full max-w-lg bg-gray-700 px-4">
-      <Fieldset className="space-y-6 rounded-xl bg-white/5 p-6 sm:p-10">
+    <div className="w-full max-w-lg px-4">
+      <Fieldset className="space-y-6 rounded-xl p-6 sm:p-10">
         <Legend className="text-base/7 font-semibold text-white">
           Organization onboarding
         </Legend>
         <Field>
           <Label className="text-sm/6 font-medium text-white">Legal name</Label>
+          <Description className="text-sm/6 text-white/50">
+            The registered name of your company.
+          </Description>
           <Input
+            placeholder="AxelCorp AB"
             className={clsx(
               "mt-3 block w-full rounded-lg border-none bg-white/5 px-3 py-1.5 text-sm/6 text-white",
               "focus:outline-none data-[focus]:outline-2 data-[focus]:-outline-offset-2 data-[focus]:outline-white/25",
@@ -40,7 +49,7 @@ const OrganizationForm = () => {
                 "*:text-black",
               )}
             >
-              <option>Sweden</option>
+              <option value="SE">Sweden</option>
             </Select>
             <ChevronDownIcon
               className="group pointer-events-none absolute right-2.5 top-2.5 size-4 fill-white/60"
@@ -48,9 +57,17 @@ const OrganizationForm = () => {
             />
           </div>
         </Field>
+
+        <Button
+          onClick={handleClick}
+          className="inline-flex animate-pulse items-center gap-2 rounded-md bg-gray-700 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-inner shadow-white/10 focus:outline-none data-[hover]:bg-gray-600 data-[open]:bg-gray-700 data-[focus]:outline-1 data-[focus]:outline-white"
+        >
+          <ArrowPathIcon className="h-5 w-5 animate-spin" />
+          Submit
+        </Button>
       </Fieldset>
     </div>
   );
 };
 
-export default OrganizationForm;
+export default SimpleOrganizationForm;
